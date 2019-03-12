8e3972e08d941c583bcb00084c99593c29b726e3
Mikhail korchevskiy <mkorchevskiy@ryerson.ca>
2019-03-12 14:24:39 -0400

    wip for environment

--- environment.cpp
+++ environment.cpp
@@ -1,5 +1,8 @@
 #include "environment.h"
 
+random_device r;
+default_random_engine generator{ r() };
+
 Environment::Environment(double p1, double p2, int start_state) {
 	this->p1 = p1;
 	this->p2 = p2;
@@ -13,8 +16,52 @@ int Environment::get_current_state() {
 	return current_state;
 }
 
+int Environment::hypothetical_state(int a) {
+	int hypo_state;
+	switch (a)
+	{
+		case 0:  // UP 
+			hypo_state = current_state + 10;
+			if (restricted_by_up.find(hypo_state) == restricted_by_up.end()) hypo_state = -1;
+			break;
+		case 1:  // DOWN
+			hypo_state = current_state - 10;
+			if (restricted_by_up.find(hypo_state) == restricted_by_down.end()) hypo_state = -1;
+			break;
+		case 2:  // LEFT
+			hypo_state = current_state - 1;
+			if (restricted_by_up.find(hypo_state) == restricted_by_left.end()) hypo_state = -1;
+			break;
+		case 3:  // RIGHT
+			hypo_state = current_state + 1;
+			if (restricted_by_up.find(hypo_state) == restricted_by_right.end()) hypo_state = -1;
+			break;
+		default:
+			hypo_state = -1;
+			break;
+	}
+
+	return hypo_state;
+}
+
+
+
 int Environment::get_reward(int a) {
 	int next_state = -1;
+
+
+
+	uniform_real_distribution<float> distribution(0.0f, 1.0f);
+	float chance = distribution(generator);
+	if (chance < p1) {    // chance happened to go where intended
+
+	} else if (chance < p1 + p2){ // chance of stay in place
+
+	} else if (chance < p1 + p2 + off_the_grid_adjacent){  // adjacent left
+
+	} else {	// adjacent right 
+
+	}
 
 	// TODO: finish actual state
 
--- environment.h
+++ environment.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <random>
+#include <set>
+
+using namespace std;
 
 class Environment {
 	public:
@@ -14,4 +18,9 @@ class Environment {
 		double off_the_grid_adjacent;
 		int current_state = 0;
 		int last_reward = -1;
+		int hypothetical_state(int a);
+		set<int> restricted_by_up = {40, 41, 43, 44, 45, 46, 48, 49, 90, 91, 92, 93, 94, 95, 96, 97, 98};
+		set<int> restricted_by_down = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 50, 51, 53, 54, 55, 56, 58, 59};
+		set<int> restricted_by_left = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 5, 15, 35, 45, 55, 65, 85, 95};
+		set<int> restricted_by_right = {9, 19, 39, 49, 59, 69, 89, 4, 14, 34, 44, 54, 64, 84, 94};
 };
