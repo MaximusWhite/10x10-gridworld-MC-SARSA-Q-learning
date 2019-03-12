00aeb75f945d869c0e684c7f5db10898aec969d3
Mikhail korchevskiy <mkorchevskiy@ryerson.ca>
2019-03-12 17:49:30 -0400

    added environment behaviour

--- environment.cpp
+++ environment.cpp
@@ -16,57 +16,274 @@ int Environment::get_current_state() {
 	return current_state;
 }
 
-int Environment::hypothetical_state(int a) {
+int Environment::next_state(int a) {
 	int hypo_state;
+	uniform_real_distribution<float> distribution(0.0f, 1.0f);
+	double prob = distribution(generator);
 	switch (a)
 	{
 		case 0:  // UP 
-			hypo_state = current_state + 10;
-			if (restricted_by_up.find(hypo_state) == restricted_by_up.end()) hypo_state = -1;
+			
+			if (restricted_by_up.find(current_state) != restricted_by_up.end()) {	// bumping into up
+				hypo_state = find_state_along_wall(-1); 
+			} else if (restricted_by_left.find(current_state) != restricted_by_left.end()) {  // along left wall
+				if (prob < p1) {
+					hypo_state = current_state + 10;	// move up
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state + 10 + 1; // move up scewed right
+				}
+			} else if (restricted_by_right.find(current_state) != restricted_by_right.end()) { // along right wall
+				if (prob < p1) {
+					hypo_state = current_state + 10;	// move up
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state + 10 - 1; // move up scewed left
+				}
+			} else {																		// general move
+				if (prob < p1) {
+					hypo_state = current_state + 10;	// move up
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					hypo_state = current_state + 10 - 1; // move up scewed left
+				} else {
+					hypo_state = current_state + 10 + 1; // move up scewed right
+				}
+			}
 			break;
 		case 1:  // DOWN
-			hypo_state = current_state - 10;
-			if (restricted_by_up.find(hypo_state) == restricted_by_down.end()) hypo_state = -1;
+			
+			if (restricted_by_down.find(current_state) != restricted_by_down.end()) {	// bumping into up
+				hypo_state = find_state_along_wall(-2); 
+			} else if (restricted_by_left.find(current_state) != restricted_by_left.end()) {  // along left wall
+				if (prob < p1) {
+					hypo_state = current_state - 10;	// move down
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state - 10 + 1; // move down scewed right
+				}
+			} else if (restricted_by_right.find(current_state) != restricted_by_right.end()) { // along right wall
+				if (prob < p1) {
+					hypo_state = current_state - 10;	// move down
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state - 10 - 1; // move down scewed left
+				}
+			} else {																		// general move
+				if (prob < p1) {
+					hypo_state = current_state - 10;	// move up
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					hypo_state = current_state - 10 - 1; // move down scewed left
+				} else {
+					hypo_state = current_state - 10 + 1; // move down scewed right
+				}
+			}
 			break;
 		case 2:  // LEFT
-			hypo_state = current_state - 1;
-			if (restricted_by_up.find(hypo_state) == restricted_by_left.end()) hypo_state = -1;
+
+			if (restricted_by_left.find(current_state) != restricted_by_left.end()) {	// bumping into left
+				hypo_state = find_state_along_wall(-3); 
+			} else if (restricted_by_up.find(current_state) != restricted_by_up.end()) {  // along up wall
+				if (prob < p1) {
+					hypo_state = current_state - 1;	// move left
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state - 10 - 1; // move left scewed down
+				}
+			} else if (restricted_by_down.find(current_state) != restricted_by_down.end()) { // along down wall
+				if (prob < p1) {
+					hypo_state = current_state - 1;	// move left
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state + 10 - 1; // move left scewed up
+				}
+			} else {																		// general move
+				if (prob < p1) {
+					hypo_state = current_state - 1;	// move left
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					hypo_state = current_state + 10 - 1; // move left scewed up
+				} else {
+					hypo_state = current_state - 10 - 1; // move left scewed down
+				}
+			}
 			break;
 		case 3:  // RIGHT
 			hypo_state = current_state + 1;
-			if (restricted_by_up.find(hypo_state) == restricted_by_right.end()) hypo_state = -1;
+			if (restricted_by_right.find(current_state) != restricted_by_right.end()) {	// bumping into right
+				hypo_state = find_state_along_wall(-4); 
+			} else if (restricted_by_up.find(current_state) != restricted_by_up.end()) {  // along up wall
+				if (prob < p1) {
+					hypo_state = current_state + 1;	// move right
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state - 10 + 1; // move right scewed down
+				}
+			} else if (restricted_by_down.find(current_state) != restricted_by_down.end()) { // along down wall
+				if (prob < p1) {
+					hypo_state = current_state + 1;	// move right
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else {
+					hypo_state = current_state + 10 + 1; // move right scewed up
+				}
+			} else {																		// general move
+				if (prob < p1) {
+					hypo_state = current_state + 1;	// move right
+				} else if (prob < p1 + p2) {
+					hypo_state = current_state;			// stay
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					hypo_state = current_state + 10 + 1; // move right scewed up
+				} else {
+					hypo_state = current_state - 10 + 1; // move right scewed down
+				}
+			}
 			break;
 		default:
-			hypo_state = -1;
+			hypo_state = -5;
 			break;
 	}
 
 	return hypo_state;
 }
 
-
-
-int Environment::get_reward(int a) {
-	int next_state = -1;
-
-
+int Environment::find_state_along_wall(int hypo_state) {
+	int s = -1;
 
 	uniform_real_distribution<float> distribution(0.0f, 1.0f);
-	float chance = distribution(generator);
-	if (chance < p1) {    // chance happened to go where intended
+	double prob = distribution(generator);
 
-	} else if (chance < p1 + p2){ // chance of stay in place
+	switch (hypo_state)
+	{
+		case -1:	{	// bumping into up
+			if (restricted_by_right.find(current_state) != restricted_by_right.end()) {  // up and right 
 
-	} else if (chance < p1 + p2 + off_the_grid_adjacent){  // adjacent left
+				if (prob < p1 + p2) {
+					s = current_state;  // stay
+				} else {
+					s = current_state - 1; // move left
+				}
+			} else if (restricted_by_left.find(current_state) != restricted_by_left.end()) {  // up and left
+				if (prob < p1 + p2) {
+					s = current_state; // stay
+				} else {
+					s = current_state + 1; // move right
+				}
+			} else {																		// just up
+				if (prob < p1 + p2) {
+					s = current_state; // stay
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					s = current_state - 1; // move left
+				} else {
+					s = current_state + 1; // move right 
+				}
+			}
+			break;
+		}
+		case -2:	{	// bumping into down
+			if (restricted_by_right.find(current_state) != restricted_by_right.end()) {  // down and right 
 
-	} else {	// adjacent right 
+				if (prob < p1 + p2) {
+					s = current_state; // stay
+				} else {
+					s = current_state - 1; // move left
+				}
 
+			} else if (restricted_by_left.find(current_state) != restricted_by_left.end()) {  // down and left
+				if (prob < p1 + p2) {
+					s = current_state; // stay
+				} else {
+					s = current_state + 1; // move right
+				}
+			} else {																		// just down
+				if (prob < p1 + p2) {
+					s = current_state; // stay
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					s = current_state - 1;  // move left
+				} else {
+					s = current_state + 1;  // move right
+				}
+			}
+			break;
+		}
+		case -3:	{	// bumping into left
+			if (restricted_by_up.find(current_state) != restricted_by_up.end()) {  // left and up
+
+				if (prob < p1 + p2) {
+					s = current_state;
+				} else {
+					s = current_state - 10;
+				}
+
+			} else if (restricted_by_down.find(current_state) != restricted_by_down.end()) {  // left and down
+				if (prob < p1 + p2) {
+					s = current_state;
+				} else {
+					s = current_state + 10;
+				}
+			} else {																		// just up
+				if (prob < p1 + p2) {
+					s = current_state;
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					s = current_state - 10;
+				} else {
+					s = current_state + 10;
+				}
+			}
+			break;
+		}
+		case -4:	{	// bumping into right
+			if (restricted_by_up.find(current_state) != restricted_by_up.end()) {  // right and up
+
+				if (prob < p1 + p2) {
+					s = current_state;
+				} else {
+					s = current_state - 10;
+				}
+
+			} else if (restricted_by_down.find(current_state) != restricted_by_down.end()) {  // right and down
+				if (prob < p1 + p2) {
+					s = current_state;
+				} else {
+					s = current_state + 10;
+				}
+			} else {																		// just up
+				if (prob < p1 + p2) {
+					s = current_state;
+				} else if (prob < p1 + p2 + off_the_grid_adjacent) {
+					s = current_state - 10;
+				} else {
+					s = current_state + 10;
+				}
+			}
+			break;
+		}
+		default:
+			s = -5;
+			break;
 	}
 
-	// TODO: finish actual state
+	return s;
+}
 
-	last_reward = next_state == 99 ? 100 : -1;
-	current_state = next_state;
+int Environment::get_reward(int a) {
+	int next_s = -1;
+
+	next_s = next_state(a);
+	
+	last_reward = next_s == 99 ? 100 : -1;
+	current_state = next_s;
 	return last_reward;
 }
 
--- environment.h
+++ environment.h
@@ -18,7 +18,8 @@ class Environment {
 		double off_the_grid_adjacent;
 		int current_state = 0;
 		int last_reward = -1;
-		int hypothetical_state(int a);
+		int next_state(int a);
+		int find_state_along_wall(int hypo_state);
 		set<int> restricted_by_up = {40, 41, 43, 44, 45, 46, 48, 49, 90, 91, 92, 93, 94, 95, 96, 97, 98};
 		set<int> restricted_by_down = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 50, 51, 53, 54, 55, 56, 58, 59};
 		set<int> restricted_by_left = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 5, 15, 35, 45, 55, 65, 85, 95};
